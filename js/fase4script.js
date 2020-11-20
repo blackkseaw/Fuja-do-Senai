a3302f87a007add015593b48453bd6b2420c634c
blackkseaw <blackkseaw@gmail.com>
2020-11-19 22:00:01 -0300

    19/11 - 22:00

--- js/fase4script.js
+++ js/fase4script.js
@@ -123,5 +123,5 @@ const verificar4 = function (alternativa) {
         window.location.href="gameover.html"
     }
 
-    window.location.href='./fase4.html'
+    window.location.href='./win.html'
 }
